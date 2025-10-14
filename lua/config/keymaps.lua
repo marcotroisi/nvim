e78d0888533000a03f9e02ae4551e6b84385c9d9
Marco Troisi <hello@marcotroisi.com>
2025-10-14 11:23:01 +0100

    now using oil as file explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
-vim.keymap.set(
-  "n",
-  "<leader>sp",
-  '<cmd>lua require("grug-far").grug_far({ prefills = { paths = vim.fn.expand("%") } })<CR>',
-  {
-    desc = "Search on current file",
-  }
-)
+vim.keymap.set("n", "<leader>sp", function()
+  require("grug-far").open({ prefills = { paths = vim.fn.expand("%"), search = vim.fn.expand("<cword>") } })
+end, { desc = "Search on current file" })
+
+vim.keymap.set({ "n", "x" }, "<leader>si", function()
+  require("grug-far").open({ visualSelectionUsage = "operate-within-range" })
+end, { desc = "grug-far: Search within range" })
+
+vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,26 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = true,
+    keymaps = {
+      ["Y"] = {
+        desc = "Copy filepath to system clipboard",
+        callback = function()
+          require("oil.actions").copy_entry_path.callback()
+          vim.fn.setreg("+", vim.fn.getreg(vim.v.register))
+        end,
+      },
+      ["<CR>"] = "actions.preview",
+      ["l"] = "actions.select",
+      ["H"] = "actions.toggle_hidden",
+      ["h"] = { "actions.parent", mode = "n" },
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
